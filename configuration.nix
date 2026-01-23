36790fa0e42d97a850ffe4f69f512fb47af341fb
mikhail.tsai <mikhail.tsai@gmail.com>
2026-01-23 03:58:30 -0300

    fixes

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ /etc/nixos/hardware-configuration.nix ];
+  imports = [ ./hardware-configuration.nix ];
 
   programs.ssh.startAgent = true;
   virtualisation.vmware.guest.enable = true;
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 Build OS:
 
 ```shell
+sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
+
 sudo nixos-rebuild switch --flake ./#nixos
 ```
