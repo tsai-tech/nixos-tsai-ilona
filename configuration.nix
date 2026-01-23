cc0a2a4d0f97b00fde6fc6050db708c187acd2a6
mikhail.tsai <mikhail.tsai@gmail.com>
2026-01-23 02:47:57 -0300

    changes

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
@@ -16,21 +12,20 @@
 
   # only for VMWare
   virtualisation.vmware.guest.enable = true;
+  services.openssh.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  # only for VMWare end
 
   hardware.graphics.enable = true;
-
 
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.wireless.enable = true;
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -53,13 +48,6 @@
     LC_TIME = "es_UY.UTF-8";
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mikhailtsai = {
     isNormalUser = true;
     description = "Mikhail Tsai";
@@ -67,46 +55,15 @@
     packages = with pkgs; [];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     pulseaudio
     kitty
     git 
     open-vm-tools
  ];
 
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   services.pipewire = {
     enable = true;
 
@@ -115,23 +72,19 @@
 
     pulse.enable = true;
     jack.enable = true;
-
   };
-
 
   programs.hyprland.enable = false;
 
   services.greetd = {
- enable = false;
-settings = {
-default_session = {
-command = "Hyprland";
-user = "mikhailtsai";
+    enable = false;
+    settings = {
+      default_session = {
+        command = "Hyprland";
+        user = "mikhailtsai";
+      };
+    };
   };
-};
-};
-
 
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
